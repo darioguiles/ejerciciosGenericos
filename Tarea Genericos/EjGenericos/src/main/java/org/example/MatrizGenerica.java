d9072e6e9856d4b748282cc7741263f8d9fd53b1
darioguiles <dario.guilescuesta@hotmail.com>
2023-10-05 11:02:41 +0200

    committttt

--- Tarea Genericos/EjGenericos/src/main/java/org/example/MatrizGenerica.java
+++ Tarea Genericos/EjGenericos/src/main/java/org/example/MatrizGenerica.java
@@ -8,18 +8,18 @@ public class MatrizGenerica <T>{
 
 
 
-    public MatrizGenerica(int fila, int columna) {
+    public MatrizGenerica(int fila, int columna) { //Constructor de nuestra clase
         this.fila = fila;
         this.columna = columna;
-        this.matriz = (T[][]) new Object[fila][columna];
+        this.matriz = (T[][]) new Object[fila][columna]; //Es necesario este casteo porque no podemos hacer new T[][]
     }
 
-    public void set(int fila, int columna, T elemento)
+    public void set(int fila, int columna, T elemento) //Mete un elemento en la fila o columna que elijamos
     {
         this.matriz[fila][columna] = elemento;
     }
 
-    public T get(int fila, int columna)
+    public T get(int fila, int columna) //Saca un elemento de la fila y columna elegida
     {
         T resultado = this.matriz[fila][columna];
                 return resultado;
@@ -27,11 +27,11 @@ public class MatrizGenerica <T>{
 
     public int columnas(){
         return this.columna;
-    }
+    } //devuelve el numero de columnas de la matriz
 
     public int filas(){
         return this.fila;
-    }
+    } //devuelve el numero de filas de la matriz
 
     @Override
     public String toString() {
--- Tarea Genericos/EjGenericos/src/main/java/org/example/PilaGenerica.java
+++ Tarea Genericos/EjGenericos/src/main/java/org/example/PilaGenerica.java
@@ -2,16 +2,16 @@ package org.example;
 import java.util.LinkedList;
 
 public class PilaGenerica <T>{
-    private LinkedList<T> lista= new LinkedList<>();
+    private LinkedList<T> lista= new LinkedList<>(); //Creacion e instancion de nuestra pila de elementos genericos
 
     public PilaGenerica() {
     }
 
     public LinkedList<T> getLista() {
         return lista;
-    }
+    } //Metodo de clase que nos devuelve la lista
 
-    public boolean estaVacia(){
+    public boolean estaVacia(){ //Metodo de clase que nos devuelve True si la lista está vacia o False si contiene
         boolean resultado=false;
 
         if (this.lista.isEmpty()) {
@@ -21,14 +21,14 @@ public class PilaGenerica <T>{
         return resultado;
     }
 
-    public T extraer()
+    public T extraer() //Este metodo extrae el último elemento y lo saca de la lista (es una pila por lo que se rige por el F.I.L.O.)
     {
         T resultado = this.lista.getLast();
         this.lista.remove(resultado);
         return resultado;
     }
 
-    public T primero()
+    public T primero() //Devuelve el ultimo elemento de la lista pero sin sacarlo
     {
         T resultado = this.lista.getFirst();
         return resultado;
@@ -37,7 +37,7 @@ public class PilaGenerica <T>{
     public void aniadir(T o)
     {
         this.lista.add(0,o);
-    }
+    } //Añade un elemento (al principio de la lista)
 
     @Override
     public String toString() {
